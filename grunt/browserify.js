d75bee31e579a5e190af06fdc2b9c679c15b1753
Kirill Hlebnikov <su.ilveran@gmail.com>
2016-08-08 11:24:31 +0300

    browserify task added

---
+++ grunt/browserify.js
@@ -0,0 +1,15 @@
+module.exports = {
+    develop: {
+        files: {
+            '<%= project.js %>/<%= project.name %>-run.js' : [
+                '<%= project.blocks %>/**/js/*.js',
+                '<%= project.app %>/*.js'
+            ]
+        },
+        options: {
+            browserifyOptions: {
+                debug: true
+            }
+        }
+    }
+};
