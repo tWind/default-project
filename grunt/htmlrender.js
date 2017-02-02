a6aa5acbbd72630fb58166d8d2418cfe3d71c433
Михаил Хлебников <mihailhlebnikov@MacAir.local>
2017-02-02 23:25:14 +0300

    Правка задачи htmlrender. Форматирование кода и комментариев

--- grunt/htmlrender.js
+++ grunt/htmlrender.js
@@ -1,16 +1,16 @@
 module.exports = {
     htmlrender: {
         options: {
-            src:  ['<%= project.blocks %>/**/*.html', 'templates/sources/*.html', '<%= project.blocks %>/**/**/*.html'], //Все возможные пути, для поиска TPL
-            vars: {} //бъявление переменных, доступных внутри TPL
+            src: ['<%= project.blocks %>/**/*.html', 'templates/sources/*.html', '<%= project.blocks %>/**/**/*.html'], //Все возможные пути, для поиска TPL
+            vars: {} // Объявление переменных, доступных внутри TPL
         },
-        files:   [
+        files: [
             {
-                expand: true, //вклюбчает и отключает указание пути в свойстве cwd
-                cwd:    'templates/sources', // путь к файлу сборщика TPL
-                src:    ['main.html', 'basket.html'], // Название файлов сборщика TPL, важно! Эти файлы должны так же находиться в options.src
-                dest:   'templates', // Папка для собранных файлов
-                ext:    '.html' //ограничение набора файлов для построения карты маршрутов
+                expand: true, // Включает и отключает указание пути в свойстве cwd
+                cwd: 'templates/sources', // Путь к файлу сборщика TPL
+                src: ['main.html'], // Название файлов сборщика TPL, важно! Эти файлы должны так же находиться в options.src
+                dest: 'templates', // Папка для собранных файлов
+                ext: '.html' // Ограничение набора файлов для построения карты маршрутов
             }
         ]
     }
